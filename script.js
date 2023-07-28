7c65615630b6f0263af91e24d15de21de97a594d
AltmannPeter <134623745+AltmannPeter@users.noreply.github.com>
2023-07-28 09:18:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-function addNumbers() {
-  const num1 = parseFloat(document.getElementById("num1").value);
-  const num2 = parseFloat(document.getElementById("num2").value);
+function calculateBitVectorSize() {
+  const coinBias = parseFloat(document.getElementById("coin-bias").value);
 
-  if (isNaN(num1) || isNaN(num2)) {
-    document.getElementById("result").textContent = "Please enter valid numbers.";
+  if (isNaN(coinBias) || coinBias < 0 || coinBias > 1) {
+    document.getElementById("result").textContent = "Please enter a valid coin bias (between 0 and 1).";
   } else {
-    const sum = num1 + num2;
-    document.getElementById("result").textContent = `The sum is: ${sum}`;
+    const bitVectorSize = 1 << 20;
+    const probabilityOfOne = coinBias;
+    const expectedOnes = bitVectorSize * probabilityOfOne;
+    const sizeInBytes = expectedOnes / 8;
+
+    const formattedSize = sizeInBytes.toLocaleString(undefined, { maximumFractionDigits: 2 });
+    document.getElementById("result").textContent = `Estimated size of the compressed bit vector: ${formattedSize} bytes`;
   }
 }
