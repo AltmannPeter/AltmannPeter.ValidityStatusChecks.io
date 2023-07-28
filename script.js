6db23118d2ed3c5513476849172256ca4f4768b8
AltmannPeter <134623745+AltmannPeter@users.noreply.github.com>
2023-07-28 10:17:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,47 @@ function generateBitVector(length, probabilityOfOne) {
 initializeTableWithSampleData();
 
 function calculateBitVectorAnalysis() {
-  // Rest of the code remains the same...
+  const coinBiasInput = document.getElementById("coin-bias");
+  const coinBias = parseFloat(coinBiasInput.value.trim());
+
+  if (isNaN(coinBias) || coinBias < 0 || coinBias > 100) {
+    document.getElementById("result").textContent = "Please enter a valid probability of 1 (between 0 and 100%).";
+  } else {
+    // Check for duplicate entries
+    const exists = analysisData.some(data => data.bias === coinBias);
+    if (exists) {
+      document.getElementById("result").textContent = "This value already exists in the table.";
+      return;
+    }
+
+    const probabilityOfOne = coinBias / 100;
+    const probabilityOfZero = 1 - probabilityOfOne;
+    const shannonEntropy = -(probabilityOfOne * Math.log2(probabilityOfOne) + probabilityOfZero * Math.log2(probabilityOfZero));
+    const formattedEntropy = shannonEntropy.toLocaleString(undefined, { maximumFractionDigits: 4 });
+
+    // Calculate the number of bits required to represent the bit vector
+    const numBits = 1 << 20;
+
+    // Generate the bit vector
+    const bitVector = generateBitVector(numBits, probabilityOfOne);
+
+    // Gzip compress the bit vector with maximum compression (level 9)
+    const compressedData = pako.gzip(bitVector, { level: 9 });
+
+    // Get the size of the compressed data in bytes
+    const compressedSizeInBytes = compressedData.length;
+
+    analysisData.push({ bias: coinBias, entropy: shannonEntropy, compressedSize: compressedSizeInBytes });
+
+    // Sort the table by Shannon entropy in descending order
+    analysisData.sort((a, b) => b.entropy - a.entropy);
+
+    // Update the table
+    updateTable();
+
+    document.getElementById("result").textContent = ""; // Clear any previous messages
+    coinBiasInput.value = ""; // Clear the input field after adding to the table
+  }
 }
 
 function updateTable() {
