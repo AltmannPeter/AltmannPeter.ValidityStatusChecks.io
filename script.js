c8cb6b896f1516af5feb4fcdde22a4c1aa87f039
AltmannPeter <134623745+AltmannPeter@users.noreply.github.com>
2023-07-28 09:38:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-body {
-  font-family: Arial, sans-serif;
+let entropyData = [];
+
+function calculateShannonEntropy() {
+  const coinBias = parseFloat(document.getElementById("coin-bias").value);
+
+  if (isNaN(coinBias) || coinBias < 0 || coinBias > 100) {
+    document.getElementById("result").textContent = "Please enter a valid probability of 1 (between 0 and 100%).";
+  } else {
+    // Check for duplicate entries
+    const exists = entropyData.some(data => data.bias === coinBias);
+    if (exists) {
+      document.getElementById("result").textContent = "This value already exists in the table.";
+      return;
+    }
+
+    const probabilityOfOne = coinBias / 100;
+    const probabilityOfZero = 1 - probabilityOfOne;
+    const shannonEntropy = -(probabilityOfOne * Math.log2(probabilityOfOne) + probabilityOfZero * Math.log2(probabilityOfZero));
+    const formattedEntropy = shannonEntropy.toLocaleString(undefined, { maximumFractionDigits: 4 });
+
+    entropyData.push({ bias: coinBias, entropy: shannonEntropy });
+
+    // Sort the table and update
+    entropyData.sort((a, b) => b.entropy - a.entropy);
+    updateTable();
+
+    document.getElementById("result").textContent = ""; // Clear any previous messages
+  }
 }
 
-.container {
-  max-width: 600px;
-  margin: 0 auto;
-  padding: 20px;
-}
+function updateTable() {
+  const tableBody = document.getElementById("entropy-table");
+  tableBody.innerHTML = "";
 
-table {
-  width: 100%;
-  border-collapse: collapse;
-  border: 1px solid #ddd;
-}
+  entropyData.forEach((data) => {
+    const row = document.createElement("tr");
+    const biasCell = document.createElement("td");
+    const entropyCell = document.createElement("td");
 
-th, td {
-  padding: 8px;
-  text-align: left;
-  border: 1px solid #ddd;
+    biasCell.textContent = data.bias.toFixed(2) + "%";
+    entropyCell.textContent = data.entropy.toFixed(4);
+
+    row.appendChild(biasCell);
+    row.appendChild(entropyCell);
+    tableBody.appendChild(row);
+  });
 }
