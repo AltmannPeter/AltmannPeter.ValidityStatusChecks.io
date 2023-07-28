49fe70429f58391cfa0101dc038bd4154aa099eb
AltmannPeter <134623745+AltmannPeter@users.noreply.github.com>
2023-07-28 13:48:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ function initializeTableWithSampleData() {
 
     analysisData.push({ bias: bias, entropy: shannonEntropy, compressedSize: compressedSizeInBytes, asr: asr });
   });
-  generateGraph(); // Call generateGraph after updating the table
 }
 
 // Function to generate a random bit vector
