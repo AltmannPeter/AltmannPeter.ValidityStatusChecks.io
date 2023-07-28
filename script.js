de55fa28c6e55558477e3b9866ad3a5da00a9b96
AltmannPeter <134623745+AltmannPeter@users.noreply.github.com>
2023-07-28 13:43:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ function initializeTableWithSampleData() {
 
     analysisData.push({ bias: bias, entropy: shannonEntropy, compressedSize: compressedSizeInBytes, asr: asr });
   });
+  generateGraph(); // Call generateGraph after updating the table
 }
 
 // Function to generate a random bit vector
@@ -39,7 +40,6 @@ function generateBitVector(length, probabilityOfOne) {
 
 // Call the function to initialize the table with sample data
 initializeTableWithSampleData();
-generateGraph(); // Call generateGraph after updating the table
 
 function calculateBitVectorAnalysis() {
   const coinBiasInput = document.getElementById("coin-bias");
