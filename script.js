59f92fdea968ae040619eb4ed6d6822b1b697422
AltmannPeter <134623745+AltmannPeter@users.noreply.github.com>
2023-07-28 09:29:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ let entropyData = [];
 function calculateShannonEntropy() {
   const coinBias = parseFloat(document.getElementById("coin-bias").value);
 
-  if (isNaN(coinBias) || coinBias < 0 || coinBias > 1) {
-    document.getElementById("result").textContent = "Please enter a valid coin bias (between 0 and 1).";
+  if (isNaN(coinBias) || coinBias < 0 || coinBias > 100) {
+    document.getElementById("result").textContent = "Please enter a valid probability of 1 (between 0 and 100%).";
   } else {
-    const probabilityOfZero = 1 - coinBias;
-    const shannonEntropy = -(coinBias * Math.log2(coinBias) + probabilityOfZero * Math.log2(probabilityOfZero));
+    const probabilityOfOne = coinBias / 100;
+    const probabilityOfZero = 1 - probabilityOfOne;
+    const shannonEntropy = -(probabilityOfOne * Math.log2(probabilityOfOne) + probabilityOfZero * Math.log2(probabilityOfZero));
     const formattedEntropy = shannonEntropy.toLocaleString(undefined, { maximumFractionDigits: 4 });
 
     entropyData.push({ bias: coinBias, entropy: shannonEntropy });
@@ -27,7 +28,7 @@ function updateTable() {
     const biasCell = document.createElement("td");
     const entropyCell = document.createElement("td");
 
-    biasCell.textContent = data.bias.toFixed(2);
+    biasCell.textContent = data.bias.toFixed(2) + "%";
     entropyCell.textContent = data.entropy.toFixed(4);
 
     row.appendChild(biasCell);
