78af70bc502771c5341090fff3af6663f64060e8
AltmannPeter <134623745+AltmannPeter@users.noreply.github.com>
2023-07-28 09:26:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let entropyData = [];
+
 function calculateShannonEntropy() {
   const coinBias = parseFloat(document.getElementById("coin-bias").value);
 
@@ -6,8 +8,30 @@ function calculateShannonEntropy() {
   } else {
     const probabilityOfZero = 1 - coinBias;
     const shannonEntropy = -(coinBias * Math.log2(coinBias) + probabilityOfZero * Math.log2(probabilityOfZero));
-
     const formattedEntropy = shannonEntropy.toLocaleString(undefined, { maximumFractionDigits: 4 });
-    document.getElementById("result").textContent = `Shannon Entropy: ${formattedEntropy}`;
+
+    entropyData.push({ bias: coinBias, entropy: shannonEntropy });
+
+    updateTable();
   }
+}
+
+function updateTable() {
+  const tableBody = document.getElementById("entropy-table");
+  tableBody.innerHTML = "";
+
+  entropyData.sort((a, b) => b.entropy - a.entropy);
+
+  entropyData.forEach((data) => {
+    const row = document.createElement("tr");
+    const biasCell = document.createElement("td");
+    const entropyCell = document.createElement("td");
+
+    biasCell.textContent = data.bias.toFixed(2);
+    entropyCell.textContent = data.entropy.toFixed(4);
+
+    row.appendChild(biasCell);
+    row.appendChild(entropyCell);
+    tableBody.appendChild(row);
+  });
 }
