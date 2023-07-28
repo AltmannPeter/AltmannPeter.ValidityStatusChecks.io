0f191a79a3f1c057ffd7cc932d7a1f07c4f5a93a
AltmannPeter <134623745+AltmannPeter@users.noreply.github.com>
2023-07-28 09:22:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-function calculateBitVectorSize() {
+function calculateShannonEntropy() {
   const coinBias = parseFloat(document.getElementById("coin-bias").value);
 
   if (isNaN(coinBias) || coinBias < 0 || coinBias > 1) {
     document.getElementById("result").textContent = "Please enter a valid coin bias (between 0 and 1).";
   } else {
-    const bitVectorSize = 1 << 20;
-    const probabilityOfOne = coinBias;
-    const expectedOnes = bitVectorSize * probabilityOfOne;
-    const sizeInBytes = expectedOnes / 8;
+    const probabilityOfZero = 1 - coinBias;
+    const shannonEntropy = -(coinBias * Math.log2(coinBias) + probabilityOfZero * Math.log2(probabilityOfZero));
 
-    const formattedSize = sizeInBytes.toLocaleString(undefined, { maximumFractionDigits: 2 });
-    document.getElementById("result").textContent = `Estimated size of the compressed bit vector: ${formattedSize} bytes`;
+    const formattedEntropy = shannonEntropy.toLocaleString(undefined, { maximumFractionDigits: 4 });
+    document.getElementById("result").textContent = `Shannon Entropy: ${formattedEntropy}`;
   }
 }
